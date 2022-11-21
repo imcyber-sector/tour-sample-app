84ee064db05ae92b0fa30aac9a0ffb99616ad135
imcyber-sector <71095875+imcyber-sector@users.noreply.github.com>
2022-11-21 08:25:11 +0100

    Add files via upload

---
+++ src/stores/productStore.ts
@@ -0,0 +1,5 @@
+import { writable } from 'svelte/store';
+
+export const ProductListStore = writable([]);
+export const ProductDetailStore = writable({});
+export const ContactDetailStore = writable({});
