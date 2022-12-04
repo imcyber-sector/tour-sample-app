077a38d988ca6a989cdcabb073f255510b35ad59
imcyber-sector <71095875+imcyber-sector@users.noreply.github.com>
2022-12-04 23:18:42 +0100

    Delete src/routes directory

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import "../app.postcss";
-  import Header  from './header/Header.svelte';
-</script>
-
-<Header />
-<slot></slot>
--- src/routes/about/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    // import Header  from '../header/Header.svelte';
-</script>
-
-<!-- <Header /> -->
